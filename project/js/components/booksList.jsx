4509997e8141e063016e042bcaebcaaaab4816c5
yuriy <yuriy.borbich@pmscloud.com>
2015-08-11 16:00:34 +0300

    auto2 in app2

---
+++ project/js/components/booksList.jsx
@@ -0,0 +1,17 @@
+/**
+ * Created by АлКаШ on 11.08.2015.
+ */
+let React = require('react');
+let mui = require('material-ui');
+let  BooksList = React.createClass({
+    render() {
+        return (
+           <div>
+           </div>
+        );
+
+    }
+
+});
+
+module.exports = BooksList;
--- project/js/components/main.jsx
+++ project/js/components/main.jsx
@@ -8,6 +8,7 @@ let Colors = mui.Styles.Colors;
 let Title = require("./title.jsx");
 let LeftNav = require("./menu.jsx");
 var Reflux = require('reflux');
+var BooksList = require('./booksList.jsx');
 var ReactRouter = require('react-router');
 let Main = React.createClass({
     mixins: [ ReactRouter.State ],
@@ -27,42 +28,7 @@ let Main = React.createClass({
                 <Title/>
             </nav>
         </div>
-        <div className="row" id = "main-menu"> </div>
-                <div className="container">
-                    <div className="jumbotron">
-                        <h1>Заголовок Jumbotron</h1>
-                        <p className="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                        <p id  = "material-desing-button"></p>
-                    </div>
-
-                    <div className="row marketing">
-                        <div className="col-lg-6">
-                            <h4>Підзаголовок</h4>
-                            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-                            <h4>Підзаголовок</h4>
-                            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-                            <h4>Підзаголовок</h4>
-                            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                        </div>
-
-                        <div className="col-lg-6">
-                            <h4>Підзаголовок</h4>
-                            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-                            <h4>Підзаголовок</h4>
-                            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-                            <h4>Підзаголовок</h4>
-                            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                        </div>
-                    </div>
-                    <footer className="footer">
-                        <p>&copy; JiJingLo 2015</p>
-                    </footer>
-
-                </div>
+            <BooksList />
         </div>
         );
       }
